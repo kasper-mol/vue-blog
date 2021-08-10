b57abc649870ff7a590ff9252746dfa80dad5271
kasper-mol <kaspermol1994@gmail.com>
2021-08-10 12:35:19 +0200

    added Blogpost component for homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,43 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost
+      :post="post"
+      v-for="(post, index) in sampleBlogPost"
+      :key="index"
+    />
+  </div>
 </template>
 
 <script>
+import BlogPost from '../components/BlogPost.vue';
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    BlogPost,
+  },
+  data() {
+    return {
+      welcomeScreen: {
+        title: 'Welcome!',
+        blogPost:
+          'Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!',
+        welcomeScreen: true,
+        photo: 'coding',
+      },
+      sampleBlogPost: [
+        {
+          title: 'Lorem Ipsum title',
+          blogHTML: 'This is just some filler text',
+          blogCoverPhoto: 'beautiful-stories',
+        },
+        {
+          title: 'Lorem Ipsum title',
+          blogHTML: 'This is just some filler text',
+          blogCoverPhoto: 'designed-for-everyone',
+        },
+      ],
+    };
+  },
 };
 </script>
